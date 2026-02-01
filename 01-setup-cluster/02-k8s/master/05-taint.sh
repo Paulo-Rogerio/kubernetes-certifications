bdfee5fb547ab36c698e7ee344d42e98084df8a0
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-01 09:44:13 -0300

    Pre-commit

--- 01-setup-cluster/02-k8s/master/05-taint.sh
+++ 01-setup-cluster/02-k8s/master/05-taint.sh
@@ -5,4 +5,9 @@ echo "***********************************************************************"
 echo "* Não agenda PODs No Control Plane                                     *"
 echo "***********************************************************************"
 echo
-kubectl taint nodes master-1 node-role.kubernetes.io/control-plane-
+nodes=$(kubectl get nodes -o name | awk -F/ '/^node\/master/ {print $2}')
+
+for i in ${nodes[@]}
+do
+  kubectl taint nodes ${i} node-role.kubernetes.io/control-plane-
+done
