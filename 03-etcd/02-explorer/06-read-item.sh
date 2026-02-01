63c3f5a338d62a838da45d801a49e9f411444222
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-01 15:23:22 -0300

    Etcd

--- 03-etcd/02-explorer/06-read-item.sh
+++ 03-etcd/02-explorer/06-read-item.sh
@@ -3,11 +3,17 @@
 source /root/etcdctl.env
 
 echo "---------"
+etcdctl get chave1 --prefix -w simple
+
 etcdctl get chave1 --prefix -w json | jq .
 
 echo "---------"
+etcdctl get chave2 --prefix -w simple
+
 etcdctl get chave2 --prefix -w json | jq .
 
 echo "---------"
+etcdctl get chave3 --prefix -w simple
+
 etcdctl get chave3 --prefix -w json | jq .
 echo "---------"
