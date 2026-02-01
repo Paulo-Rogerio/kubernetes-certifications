ef77f9ffa3af3c84d5d975b99ce0e0fa9e48ff23
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-01 15:53:53 -0300

    Correcoes

--- 02-k8s/worker/04-join.sh
+++ 02-k8s/worker/04-join.sh
@@ -6,3 +6,22 @@ echo "***********************************************************************"
 echo
 
 sh /root/join-cluster/join.sh
+
+function check_pod_running(){
+    todo=true
+    while ${todo};
+    do
+      podsWorking=$(kubectl get pod -A -o custom-columns="STATUS:.status.phase" | grep -v STATUS | egrep -vc "Running|Succeeded")
+      [[ ${podsWorking} == 0 ]] && export todo=false
+      echo "Waiting Pod Health..."
+      sleep 10
+    done
+    echo "Pods Running"
+}
+
+check_pod_running
+
+echo "***********************************************************************"
+echo "* Member Add                                                          *"
+echo "***********************************************************************"
+echo
