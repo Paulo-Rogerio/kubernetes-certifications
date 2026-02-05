fde3d02d7ecd0e02427fd9ac8ab9d32eddb63417
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-02-05 00:13:57 -0300

    Etcd static pod

--- CKA/02-cluster-kubernetes/professional/01-etcd/07-start-master02.sh
+++ CKA/02-cluster-kubernetes/professional/01-etcd/07-start-master02.sh
@@ -4,10 +4,18 @@ NAME=$(hostname -s)
 
 mkdir -p /root/kubeadmcfg-etcd
 
-scp -r root@master01:/root/kubeadmcfg-etcd/${NAME}/certs /root/kubeadmcfg-etcd
+scp -r root@master01:/root/kubeadmcfg-etcd/${NAME}/certs /root/kubeadmcfg-etcd/${NAME}
 
 mkdir -p /etc/kubernetes/pki/etcd/
 
 cp -ra /root/kubeadmcfg-etcd/${NAME}/certs/pki/etcd /etc/kubernetes/pki/
 
 kubeadm init phase etcd local --config=/root/kubeadmcfg-etcd/${NAME}/kubeadmcfg.yaml
+
+
+cat <<EOF | tee /etc/crictl.yaml
+runtime-endpoint: unix:///run/containerd/containerd.sock
+image-endpoint: unix:///run/containerd/containerd.sock
+timeout: 10
+debug: false
+EOF
